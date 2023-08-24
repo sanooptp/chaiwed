c72d9c24002ebcfcddc28bc69ab3060e805d1043
Sanoop tp <isanooptp@gmail.com>
2023-08-24 12:11:40 +0530

    third commit

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -54,7 +54,7 @@ function startTimer(endDate, display) {
   function typeText() {
     if (lineIndex < textArray.length) {
       const currentLine = textArray[lineIndex];
-      typewriter.textContent = currentLine.substring(0, charIndex);
+      // typewriter.textContent = currentLine.substring(0, charIndex);
       charIndex++;
       if (charIndex > currentLine.length) {
         charIndex = 0;
@@ -85,3 +85,41 @@ document.getElementById('addToGoogleCalendarButton').addEventListener('click', f
 
     window.open(googleCalendarUrl, '_blank');
   });
+
+
+  const carouselItems = document.querySelectorAll('.carousel-item');
+ let currentItem = 0;
+ 
+ function showNextItem() {
+     carouselItems[currentItem].style.opacity = '0';
+     currentItem = (currentItem + 1) % carouselItems.length;
+     carouselItems[currentItem].style.opacity = '1';
+ }
+ 
+ setInterval(showNextItem, 3000); // Change image every 3 seconds
+ 
+ 
+
+ const image = document.querySelector('.flower3');
+
+// Function to check if an element is in the viewport
+function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Function to apply the animation class when the image is in the viewport
+function animateImageOnScroll() {
+    if (isElementInViewport(image)) {
+        image.classList.add('animate');
+        window.removeEventListener('scroll', animateImageOnScroll);
+    }
+}
+
+// Add a scroll event listener to trigger the animation
+window.addEventListener('scroll', animateImageOnScroll);
